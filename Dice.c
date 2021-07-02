771ad2d695a95ecf680de082f49bd0acbe4c7caf
s1280135 <s1280135@std4dc37>
2021-07-02 12:14:15 +0900

    Initial revision

---
+++ Dice.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+int main(){
+  int i,j,n,sum=0;
+  
+  srand((unsigned)time(NULL));
+	
+  for(n=0;n<2;++n){
+    
+    i = rand()%6+1;
+    
+    j = rand()%6+1;
+    
+    sum=i+j;
+  }
+  
+  printf("Rolling the dice...\n");
+  printf("Dice 1：%d\n",i);
+  printf("Dice 2：%d\n",j);
+  printf("Total value: %d\n",sum);
+  
+  return 0;
+  
+}
