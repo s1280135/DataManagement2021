df2d0d848bf327b520750f2ff504e76ec5a19afe
FUJITA Shogo <s1280135@linsv213.u-aizu.ac.jp>
2021-07-08 22:23:50 +0900

    Won/lost message modified

--- Dice.c
+++ Dice.c
@@ -27,10 +27,10 @@ int main(){
   printf("Total value: %d\n",sum);
   
   if(sum>7){
-    printf("You won!\n");
+    printf("%s won!\n",name);
   }
   else{
-    printf("You lost!\n");
+    printf("%s lost!\n",name);
   }
   return 0;
 }
